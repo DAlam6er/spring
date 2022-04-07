c897067f3abf8d17a9a76e065d7291e8328337f5
Anton Pashkin <anton.zep@gmail.com>
2022-04-07 15:46:09 +0300

    add pattern examples

--- dbJDBC/src/main/java/ru/specialist/dao/Course.java
+++ dbJDBC/src/main/java/ru/specialist/dao/Course.java
@@ -2,30 +2,35 @@ package ru.specialist.dao;
 
 import java.io.Serializable;
 
+// объектная обёртка для таблицы Course
 public class Course implements Serializable {
 	
 	private int id;
 	private String title;
 	private int length;
 	private String description;
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public int getLength() {
 		return length;
 	}
 	public void setLength(int length) {
 		this.length = length;
 	}
+
 	public String getDescription() {
 		return description;
 	}
--- dbJDBC/src/main/java/ru/specialist/dao/CourseDAO.java
+++ dbJDBC/src/main/java/ru/specialist/dao/CourseDAO.java
@@ -2,12 +2,13 @@ package ru.specialist.dao;
 
 import java.util.List;
 
+// интерфейс, описывающий репозиторий
 public interface CourseDAO {
 	
 	Course findById(int id);
 	List<Course> findAll();
 	
-	// CRUD
+	// CRUD(create, update, delete)
 	List<Course> findByTitle(String title);
 	void insert(Course course);
 	void update(Course course);
