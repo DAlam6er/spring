5f22f4fa50726d51117f1337daf96eba8cca523f
Anton Pashkin <anton.zep@gmail.com>
2022-04-08 08:35:00 +0300

    modified dbHibernate HibernateCourseDAO

--- dbHibernate/src/main/java/ru/specialist/dao/HibernateCourseDAO.java
+++ dbHibernate/src/main/java/ru/specialist/dao/HibernateCourseDAO.java
@@ -3,17 +3,22 @@ package ru.specialist.dao;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaQuery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.query.Query;
 import org.hibernate.SessionFactory;
 
 @Transactional
 @Repository("courseDao")
 public class HibernateCourseDAO implements CourseDAO
 {
+    // журналирование
     private static final Log LOG = LogFactory.getLog(HibernateCourseDAO.class);
     // данная реализация репозитория жёстко привязана к ORM Hibernate
     // из-за SessionFactory
@@ -23,8 +28,12 @@ public class HibernateCourseDAO implements CourseDAO
         return sessionFactory;
     }
 
-    // инжектим property sessionFactory, связывая его с одноименным bean
+    // инжектим property sessionFactory, связывая его с одноименным bean,
+    // сконфигурированным в XML
+    // @Resource - аннотация из javax.annotation
     @Resource(name="sessionFactory")
+    //@Autowired
+    //@Value("#{'sessionFactory'}")
     public void setSessionFactory(SessionFactory sessionFactory)
     {
         this.sessionFactory = sessionFactory;
@@ -42,6 +51,7 @@ public class HibernateCourseDAO implements CourseDAO
     @Transactional(readOnly = true)
     public List<Course> findAll()
     {
+        // depricated since Hibernate 5
         return getSessionFactory().getCurrentSession().
             createQuery("from Course c").list(); // HQL
     }
@@ -49,6 +59,7 @@ public class HibernateCourseDAO implements CourseDAO
     @Override
     public List<Course> findByName(String title)
     {
+        // depricated since Hibernate 5
         return getSessionFactory().getCurrentSession().
             createQuery("from Course x where x.title LIKE :search")
             .setParameter("search", "%"+title.trim()+"%")
@@ -58,6 +69,7 @@ public class HibernateCourseDAO implements CourseDAO
     @Override
     public void insert(Course course)
     {
+        // depricated since Hibernate 6
         getSessionFactory().getCurrentSession().save(course);
         LOG.info("Course saved with id: " + course.getId());
     }
@@ -65,6 +77,7 @@ public class HibernateCourseDAO implements CourseDAO
     @Override
     public void update(Course course)
     {
+        // depricated since Hibernate 6
         getSessionFactory().getCurrentSession().update(course);
         LOG.info("Course updated with id: " + course.getId());
     }
@@ -74,8 +87,8 @@ public class HibernateCourseDAO implements CourseDAO
     {
         Course c = new Course();
         c.setId(id);
+        // depricated since Hibernate 6
         getSessionFactory().getCurrentSession().delete(c);
         LOG.info("Course deleted with id: " + c.getId());
     }
-
 }
